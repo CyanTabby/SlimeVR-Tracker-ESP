5bd167912d935ae5dffc8708024caa8ed958d600
Cyan Tabby <cyantabby@gmail.com>
2023-06-21 21:03:19 -1000

    Revert "Update defines.h"
    This reverts commit d33d26cd0b33f0ed7a40367641fbd9e7716cace7.

--- src/defines.h
+++ src/defines.h
@@ -27,10 +27,10 @@
 
 // Set parameters of IMU and board used
 #define IMU IMU_BNO085
-#define SECOND_IMU IMU_BNO085
-#define BOARD BOARD_WEMOSD1MINI
-#define IMU_ROTATION DEG_0
-#define SECOND_IMU_ROTATION DEG_0
+#define SECOND_IMU IMU
+#define BOARD BOARD_SLIMEVR
+#define IMU_ROTATION DEG_270
+#define SECOND_IMU_ROTATION DEG_270
 
 // Battery monitoring options (comment to disable):
 //   BAT_EXTERNAL for ADC pin, 
